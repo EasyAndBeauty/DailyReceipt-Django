02af3db297bcc9a9f2c58fd7c479f657b4f33046
Madang Garden <chooi9522@gmail.com>
2025-02-08 11:01:37 +0900

    wip
    (cherry picked from commit a676d005efabe3ca3cdbc01b0e62c8633b9bb666)

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'authentication'
-
-urlpatterns = [
-    path('test-token/', views.test_token, name='test-token'),
-    path('validate-token/', views.validate_token, name='validate-token'),
-]
