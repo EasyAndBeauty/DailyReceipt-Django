69018b4ce4c5a1776338aa274b2394db39e07932
Madang Garden <chooi9522@gmail.com>
2025-02-16 21:31:43 +0900

    revert: rebase 복구

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,5 +1,3 @@
-"""swagger_auto_schema를 사용하여 API 문서화"""
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
