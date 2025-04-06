72cd850ded0c329fc4751cbcb6b060c3435e69c1
Madang Garden <chooi9522@gmail.com>
2025-04-06 22:23:08 +0900

    fix: 테스트 추가

--- apps/receipt/urls.py
+++ apps/receipt/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.receipt_create, name="receipt_create"),
-    path("list/", views.pinned_receipt_list, name="pinned_receipt_list"),
+    path("", views.receipt_create, name="receipt-create"),
+    path("list/", views.pinned_receipt_list, name="pinned-receipt-list"),
 ]
